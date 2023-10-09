7e602b26fa384261a5542c9fb1ace170dd4fb413
quyenptitptit <quyenmeomeo111@gmail.com>
2023-10-09 17:38:56 +0700

    fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger-ui-dist/swagger.json");
 
 const userRouter = express.Router();
-// userRouter.use("/api-docs", swaggerUi.serve)
-// userRouter.get("/api-docs", swaggerUi.setup(swaggerDocument), userController.registerUser)
 
 userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.loginUser);
+/**
+ * @openapi
+ * /users:
+    get:
+      tags:
+        - user
+      summary: 'Get users'
+      responses:
+        '200':
+          description: 'Get all user successfully!'
+          content:
+            application/json:
+              schema:
+                type:  array
+                items:
+                  $ref: '#/components/schemas/User'          
+            application/xml:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/User' 
+        '500': 
+          description: 'Exception'
+ */
 userRouter.get("/users", userController.getAll);
+
 userRouter.get("/user/:id", userController.getUser);
 userRouter.put("/user/:id", userController.updateUser);
 userRouter.put("/updatePassword", userController.updatePassword);
