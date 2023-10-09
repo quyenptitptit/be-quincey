7b93afe15fd1e22d0ea448f55280ec0aa3d5dd31
quyenptitptit <quyenmeomeo111@gmail.com>
2023-10-09 17:52:17 +0700

    fix

--- swagger.js
+++ swagger.js
@@ -2,24 +2,37 @@ const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require('./swagger-ui-dist/swagger.json')
 
+// apis: ["./routes/*.js"],
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Quincey API",
-      description: "CRUD API ",
+      title: "Library API",
       version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
+    servers: [
+      {
+        url: "https://be-quincey.vercel.app/api/v1",
+        description: "My API Documentation",
+      },
+    ],
   },
-  // looks for configuration in specified directories
-  apis: ["./routes/*.js"],
+  // This is to call all the file
+  apis: ["src/**/*.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app, port) {
   // Swagger Page
-  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   // Documentation in JSON format
   app.get("/docs.json", (req, res) => {
